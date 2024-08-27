8a23b95618afe6b5b19ad797500c4fee5793fdc1
xiften <108333567+not-ani@users.noreply.github.com>
2024-08-26 18:20:10 -0600

    feat: seo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center overflow-x-clip pt-12 md:pt-24">
       <section className="flex flex-col items-center px-4 gap-20 sm:px-6 lg:px-8 pt-24">
         <CTA />
-
         <Partners />
       </section>
     </main>
---
+++ src/lib/config.ts
@@ -0,0 +1,1 @@
+export const siteurl = "https://creekcyber.vercel.app/";
